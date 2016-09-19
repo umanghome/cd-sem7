adcf2ab8a1112a0d114302baa88840d89a13bf7b
Umang Galaiya <umanghome@gmail.com>
2016-09-19 16:52:40 +0530

    modified p10

--- 10-ll1/code.c
+++ 10-ll1/code.c
@@ -5,6 +5,18 @@
 #define bool int
 #define true 1
 #define false 0
+#define STACK_SIZE 50
+#define MAX_TRIES 100
+
+void strrev (char* str) {
+	int length = strlen(str);
+
+	for (int i = 0; i < (length / 2); i++) {
+		char tmp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = tmp;
+	}
+}
 
 bool isTerminal (char c, char* terminals, int count) {
 	for (int i = 0; i < count; i++) {
@@ -296,8 +308,14 @@ int main () {
 					strcpy(table[i][_index], "e");
 				}
 			} else {
-				int nt_index = findIndex(c, terminals, number_t);
-				strcpy(table[i][nt_index], rules[i][0]);
+				int t_index = findIndex(c, terminals, number_t);
+				char rule_index = 0;
+				for (int k = 0; k < counters[i]; k++) {
+					if (rules[i][k][0] == c) {
+						rule_index = k;
+					}
+				}
+				strcpy(table[i][t_index], rules[i][rule_index]);
 			}
 		}
 	}
@@ -314,6 +332,97 @@ int main () {
 			printf("%s\t", table[i][j]);
 		}
 		printf("\n");
+	}
+
+	printf("Start symbol: ");
+	getchar();
+	char start = getchar();
+
+	printf("Start symbol is: %c\n", start);
+
+	char* input = (char*) malloc(sizeof(char) * 30);
+
+	printf("Input: ");
+	scanf("%s", input);
+	strcat(input, "$");
+	printf("Input string is: %s\n", input);
+
+	char* stack = (char*) malloc(sizeof(char) * (STACK_SIZE + 5));
+	for (i = 0; i < STACK_SIZE; i++) {
+		stack[i] = '\0';
+	}
+
+	int stack_top = -1;
+	stack[++stack_top] = '$';
+	stack[++stack_top] = 'A';
+
+	int ip = 0;
+
+	bool parsed = false;
+
+	printf("\nstack\t\tinput\t\taction\n\n");
+	printf("%s\t\t%s\n", stack, input);
+
+	int tries = 0;
+	while (tries < MAX_TRIES) {
+		if (parsed) break;
+
+		printf("%s\t\t", stack);
+		for (i = ip; i < strlen(input); i++) {
+			printf("%c", input[i]);
+		}
+		printf("\t\t");
+
+		int ip_index = findIndex(input[ip], terminals, number_t);
+
+		char* rule = (char*) malloc(sizeof(char) * 20);
+		for (i = 0; i < 20; i++) {
+			rule[i] = '\0';
+		}
+
+		if (isNonTerminal(stack[stack_top], non_terminals, number_nt)) {
+			char top = stack[stack_top];
+			
+			int top_index = findIndex(top, non_terminals, number_nt);
+			
+			printf("%c -> %s\n", stack[stack_top], table[top_index][ip_index]);
+
+			if (strcmp(table[top_index][ip_index], "e") != 0) {
+
+				strcpy(rule, table[top_index][ip_index]);
+				strrev(rule);
+
+				stack[stack_top--] = '\0';
+				for (i = 0; i < strlen(rule); i++) {
+					stack[++stack_top] = rule[i];
+				}
+			} else {
+				stack[stack_top--] = '\0';
+			}
+
+		} else {
+			if (stack[stack_top] == input[ip]) {
+
+				printf("matched %c\n", stack[stack_top]);
+
+				if (stack[stack_top] == '$') {
+					parsed = true;
+				}
+
+				stack[stack_top--] = '\0';
+				ip++;
+			}
+		}
+
+		tries++;
+	}
+
+	printf("\n");
+
+	if (parsed) {
+		printf("Parsing successful!\n");
+	} else {
+		printf("Parsing unsuccessful!\n");
 	}
 
 	return 0;
